457d2dfa2511b333289874e785e6576cf6c1c843
AliJP <javidanpak40@gmail.com>
2024-01-26 19:27:53 +0330

    Update

--- مهمانی دکتر افشار.py
+++ مهمانی دکتر افشار.py
@@ -8,13 +8,13 @@ while a > 0:
     l.append(a % 2)
     a = a // 2
 
-while len(l) < 64 :
+while len(l) < 64:
     l.append(0)
 
 b = int(input())
 while b > 0:
     t = int(input())
-    if l[t] == 0 :
+    if l[t] == 0:
         print("no")
     else:
         print("yes")
