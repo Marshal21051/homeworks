9f5a5e4c512c5db333f09b269234afb5f3975407
AliJP <javidanpak40@gmail.com>
2024-01-26 19:28:27 +0330

    Update

--- تله تام.py
+++ تله تام.py
@@ -1,44 +1,55 @@
 import math
 
+
 def sum(l):
     r = 0
     for k in l:
         r = r + k
     return r
 
+
 def average(l):
     r = sum(l)
     return round(r / len(l), 2)
 
+
 def min(l):
-    if len(l) == 0: return 0
+    if len(l) == 0:
+        return 0
     r = l[0]
     for k in l:
         if k < r:
             r = k
     return r
 
+
 def max(l):
-    if len(l) == 0: return 0
+    if len(l) == 0:
+        return 0
     r = l[0]
     for k in l:
         if k > r:
             r = k
     return r
 
+
 def gcd(l):
-    if len(l) == 0: return 0
+    if len(l) == 0:
+        return 0
     r = l[0]
     for k in l:
         r = math.gcd(r, k)
     return r
 
+
 def lcd(l):
-    if len(l) == 0: return 0
+    if len(l) == 0:
+        return 0
     r = l[0]
     for k in l:
         r = r * k // math.gcd(r, k)
     return r
+
 
 o = "invalid command"
 command = input()
@@ -52,11 +63,17 @@ while k != "end":
     k = input()
 
 
-if command == "sum": o = sum(l)
-elif command == "average": o = average(l)
-elif command == "lcd": o = lcd(l)
-elif command == "gcd": o = gcd(l)
-elif command == "min": o = min(l)
-elif command == "max": o = max(l)
+if command == "sum":
+    o = sum(l)
+elif command == "average":
+    o = average(l)
+elif command == "lcd":
+    o = lcd(l)
+elif command == "gcd":
+    o = gcd(l)
+elif command == "min":
+    o = min(l)
+elif command == "max":
+    o = max(l)
 
 print(o)
--- تله هوشمند.py
+++ تله هوشمند.py
@@ -1,5 +1,6 @@
 import math
 
+
 def decimal2Base(num, b):
     r = ""
     while num > 0:
@@ -8,6 +9,7 @@ def decimal2Base(num, b):
         r = (str(remainder)) + r
 
     return r
+
 
 k = input()
 ts = 0
@@ -22,7 +24,8 @@ while k != "-1 -1":
     for i in range(1, int(math.sqrt(a)) + 1):
         if a % i == 0:
             s += i
-            if i != a // i: s += a // i
+            if i != a // i:
+                s += a // i
     ts = ts + int(decimal2Base(s, b))
     k = input()
 print(ts)
